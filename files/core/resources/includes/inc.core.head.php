4bdcb69257613a9a71eb49eece38d30434a4d5de
Alejandro Romero <romero.m.alejandro@gmail.com>
2018-03-05 15:56:00 -0300

    Remote files are now locally called

--- files/core/resources/includes/inc.core.head.php
+++ files/core/resources/includes/inc.core.head.php
@@ -6,7 +6,7 @@
 <!-- Bootstrap 3.3.6 -->
 <link rel="stylesheet" href="../../../../vendors/bootstrap/bootstrap.min.css">
 <!-- Font Awesome -->
-<script src="https://use.fontawesome.com/e6af76d853.js"></script>
+<script src="../../../../vendors/fontawesome/font-awesome.min.js"></script>
 <link rel="stylesheet" href="../../../../vendors/fontawesome/font-awesome-animation.min.css">
 <!-- Ionicons -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
